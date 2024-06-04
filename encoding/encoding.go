e24f3b44830ecc35ab3baaea00bb32a87956c409
Dmitrii Korsikov <dimakors13@gmail.com>
2024-06-04 20:01:58 +0300

    modify .gitignore; realize json to yaml/ yaml to json converter

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,7 +1,12 @@
 package encoding
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -25,16 +30,78 @@ type MyEncoder interface {
 
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	// ниже реализуйте метод
-	// ...
+
+	jsonFile, err := os.ReadFile(j.FileInput)
+	if err != nil {
+		fmt.Printf("ошибка при чтении файла: %s", err.Error())
+		return err
+	}
+	j.DockerCompose = &models.DockerCompose{}
+	err = json.Unmarshal(jsonFile, j.DockerCompose)
+	if err != nil {
+		fmt.Printf("ошибка при десериализации: %s", err.Error())
+		return err
+	}
+
+	yamlData, err := yaml.Marshal(j.DockerCompose)
+	if err != nil {
+		fmt.Printf("ошибка при сериализации: %s", err.Error())
+		return err
+	}
+
+	file, err := os.Create(j.FileOutput)
+	if err != nil {
+		fmt.Printf("ошибка при создании файла: %s", err.Error())
+		return err
+	}
+
+	defer file.Close()
+
+	_, err = file.Write(yamlData)
+	if err != nil {
+		fmt.Printf("ошибка при записи данных в файл: %s", err.Error())
+		return err
+	}
 
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
-	// Ниже реализуйте метод
-	// ...
+
+	yamlFile, err := os.ReadFile(y.FileInput)
+	if err != nil {
+		fmt.Printf("ошибка при чтении файла: %s", err.Error())
+		return err
+	}
+
+	y.DockerCompose = &models.DockerCompose{}
+	err = yaml.Unmarshal(yamlFile, y.DockerCompose)
+	if err != nil {
+		fmt.Printf("ошибка при десериализации: %s", err.Error())
+		return err
+	}
+
+	jsonData, err := json.Marshal(y.DockerCompose)
+	if err != nil {
+		fmt.Printf("ошибка при сериализации: %s", err.Error())
+		return err
+	}
+
+	file, err := os.Create(y.FileOutput)
+	if err != nil {
+		fmt.Printf("ошибка при создании файла: %s", err.Error())
+		return err
+	}
+	// когда программа завершится, надо закрыть дескриптор файла
+	defer file.Close()
+
+	// записываем слайс байт в файл
+	_, err = file.Write(jsonData)
+	if err != nil {
+		fmt.Printf("ошибка при записи данных в файл: %s", err.Error())
+		return err
+	}
 
 	return nil
 }
